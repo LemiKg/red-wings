443f886c699dd11bbfa26dd79cd0fcc98216cc38
Mile Rajević <mile.rajevic@united.cloud>
2022-01-28 22:06:49 +0100

    landing page responsive

--- webpack.config.js
+++ webpack.config.js
@@ -40,8 +40,8 @@ module.exports = {
                 ]
             },
             {
-              test: /\.(png|svg|jpg|jpeg|gif)$/i,
-              type: 'asset/resource',
+              test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+              type: 'asset/resource'
             },
         ]
     }
